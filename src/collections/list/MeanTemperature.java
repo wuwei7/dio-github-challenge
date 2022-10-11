9d7dd98ca30bc8247281daf76227f8c9753fafd2
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-11 14:33:03 -0300

    Show which months had above mean temperature

--- src/collections/list/MeanTemperature.java
+++ src/collections/list/MeanTemperature.java
@@ -12,34 +12,63 @@ public class MeanTemperature {
 		
 		List<Double> temps = new ArrayList<>();
 		
-		int i = 0;
+		int i = 1;
 		while (true) {
-			if (i == 2) {
+			if (i == 7) {
 				break;
 			}
-			System.out.print("Input temperature: ");
+			System.out.print("Input temperature " + i + ": ");
 			double temp = Double.valueOf(scanner.nextLine());
 			temps.add(temp);
 			i++;
 		}
 		
 		Iterator<Double> iterator = temps.iterator();
-		double next = 0.0;
 		double sum = 0.0;
 		while (iterator.hasNext()) {
-			next = iterator.next();
-			sum += next;
+			double nextTemp = iterator.next();
+			sum += nextTemp;
 		}
 		
 		double meanTemp = sum / temps.size();
 		System.out.println("Mean temperature: " + meanTemp);
-		if (next > meanTemp) {
-			System.out.println("Temperature above mean: " + next);
+		
+		// Shows which months had above mean temperature, if any.
+		Iterator <Double> iterator1 = temps.iterator();
+		System.out.println("Months with above mean temperature:");
+		int counter = 0;
+		while (iterator1.hasNext()) {
+			int counterNoMean = 0;
+			double temp = iterator1.next();
+			if (temp > meanTemp) {
+				if (counter == 0) {
+					System.out.println("January: " + temp);
+					counterNoMean++;
+				} else if (counter == 1) {
+					System.out.println("February: " + temp);
+					counterNoMean++;
+				} else if (counter == 2) {
+					System.out.println("March: " + temp);
+					counterNoMean++;
+				} else if (counter == 3) {
+					System.out.println("April: " + temp);
+					counterNoMean++;
+				} else if (counter == 4) {
+					System.out.println("May: " + temp);
+					counterNoMean++;
+				} else if (counter == 5) {
+					System.out.println("June: " + temp);
+					counterNoMean++;
+				}
+			}
+			
+			counter++;
+			
+			if (counter == 6 && counterNoMean == 0) {
+				System.out.println("No months with above mean temperature.");
+			}
 		}
 		
-		
-		System.out.println("Temperatures: " + temps);
-
+		scanner.close();
 	}
-
 }
