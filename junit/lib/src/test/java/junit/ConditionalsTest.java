90dda85489a8273325d369d4c37e1eb6954a1d03
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-27 14:13:10 -0300

    Fix sintax @EnabledForJreRange

--- junit/lib/src/test/java/junit/ConditionalsTest.java
+++ junit/lib/src/test/java/junit/ConditionalsTest.java
@@ -3,11 +3,12 @@ package junit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 public class ConditionalsTest {
 	
 	@Test
-    @EnabledForJreRange(min = JAVA_11, max = JAVA_17)
+    @EnabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_17)
     void validarAlgoSomenteNoUsuarioWillyan() {
         Assertions.assertEquals(10, 5 + 5);
     }
